345e80279650390e34f4a0cc76a20ccaf4b13772
Paul Fitzgerald <paulfitz99@gmail.com>
2015-03-11 12:29:28 +0000

    dev branch

--- app/model/clients.rb
+++ app/model/clients.rb
@@ -4,31 +4,31 @@ class Client
 
   property :id,               Serial
   property :first_name,       String
-  property :second_name,      String
-  property :phone,            Text
-  property :email,            String,  :unique => true, :message => "This email is already taken"
-  property :password_digest,  Text
+  # property :second_name,      String
+  # property :phone,            Text
+  # property :email,            String,  :unique => true, :message => "This email is already taken"
+  # property :password_digest,  Text
 
-  has n, :appointments
+  # has n, :appointments
 
-  attr_reader :password
-  attr_accessor :password_confirmation
+  # attr_reader :password
+  # attr_accessor :password_confirmation
 
-  validates_confirmation_of :password
+  # validates_confirmation_of :password
 
-  def password=(password)
-    @password = password
-    self.password_digest = BCrypt::Password.create(password)
-  end
+  # def password=(password)
+  #   @password = password
+  #   self.password_digest = BCrypt::Password.create(password)
+  # end
 
-  def self.authenticate(email, password)
-    user = first(:email => email)
+  # def self.authenticate(email, password)
+  #   user = first(:email => email)
 
-    if user && BCrypt::Password.new(user.password_digest) == password
-      user
-    else
-      nil
-    end
-  end
+  #   if user && BCrypt::Password.new(user.password_digest) == password
+  #     user
+  #   else
+  #     nil
+  #   end
+  # end
 
 end
--- app/server.rb
+++ app/server.rb
@@ -1,8 +1,18 @@
 require 'data_mapper'
 require 'sinatra'
-require_relative 'data_mapper_setup'
+# require_relative 'data_mapper_setup'
+
 
 class HairNow < Sinatra::Base
+
+
+  env = ENV['RACK_ENV'] || 'development'
+
+  DataMapper.setup(:default, "postgres://localhost/hair_now_#{env}")
+
+  require './app/model/clients.rb'
+
+  DataMapper.finalize #after declaring models this finalises them
 
   set :root, File.dirname(__FILE__)
   set :views, Proc.new { File.join(root, "views") }
@@ -12,9 +22,20 @@ get '/' do
   erb :index
 end
 
-get '/user/new' do
+get '/client/new' do
+  @client = Client.new
+  erb :new_client
+end
 
-  erb :new_user
+post '/client/new' do
+  @client = Client.create(:first_name =>params[:first_name],
+    :last_name =>params[:last_name],
+    :email => params[:email],
+    :phone =>params[:phone],
+    :password =>params[:password],
+    :password_confirmation =>params[:password_confirmation])
+
+  erb :new_client
 end
 
 end
